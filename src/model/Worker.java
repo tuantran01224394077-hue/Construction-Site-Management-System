8b620c45a90ca2545a4ef3f6cb4ae18a88dc7ccd
Trần Ngọc Anh Tuấn <tuantran01224394077@gmail.com>
2026-09-17 12:38:56 +0700

    Update Worker.java

--- src/model/Worker.java
+++ src/model/Worker.java
@@ -9,5 +9,39 @@ package model;
  * - Cài đặt các hành vi hiển thị chi tiết và chuẩn bị dữ liệu xuất báo cáo theo yêu cầu Workshop 1.
  */
 public class Worker extends Person {
+    private String maNguoi;
+    private String hoTen;
+    private String ngaySinh;
+    private String gioiTinh;
+    private String soDienThoai;
 
+public Person() {
+   }
+  
+public Person(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai) {
+        this.maNguoi = maNguoi;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getMaNguoi() { return maNguoi; }
+    public void setMaNguoi(String maNguoi) { this.maNguoi = maNguoi; }
+ 
+    public String getHoTen() { return hoTen; }
+    public void setHoTen(String hoTen) { this.hoTen = hoTen; }
+ 
+    public String getNgaySinh() { return ngaySinh; }
+    public void setNgaySinh(String ngaySinh) { this.ngaySinh = ngaySinh; }
+ 
+    public String getGioiTinh() { return gioiTinh; }
+    public void setGioiTinh(String gioiTinh) { this.gioiTinh = gioiTinh; }
+ 
+    public String getSoDienThoai() { return soDienThoai; }
+    public void setSoDienThoai(String soDienThoai) { this.soDienThoai = soDienThoai; }
+
+   public abstract void hienThiThongTin();
+
+   public abstract List<String> chuanBiDuLieuXuatBaoCao();
 }
